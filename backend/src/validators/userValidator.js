0b6246f07b1b301f317364ca243ca2b7ef7cf607
casper <thecaspercraft@gmail.com>
2025-10-25 19:52:51 +0530

    fix: validate user create

--- backend/src/validators/userValidator.js
+++ backend/src/validators/userValidator.js
@@ -1,26 +1,25 @@
-// import joi from "joi";
+import Joi from "joi";
 import users from "#models/user"
 
-// const { object, string, required, number } = joi
 
-// const userSchema = object({
-// 	name: string().min(3).max(100).required(),
-// 	username: string().min(3).max(100).required(),
-// 	user_type: required(),
-// 	status: number().integer().required(),
-// 	parant_id: number().integer().required(),
-// 	package_id: number().integer().optional()
-// });
+const userSchema = Joi.object({
+	name: Joi.string().min(3).max(100).required(),
+	username: Joi.string().min(3).max(100).required(),
+	user_type: Joi.required(),
+	status: Joi.number().integer().required(),
+	parant_id: Joi.number().integer().required(),
+	package_id: Joi.number().integer().optional()
+});
 
 const validateUser = async (req, res, next) => {
-	// const { error } = userSchema.validate(req.body, { abortEarly: false });
-	// if (error) {
-	// 	return res.status(400).json({
-	// 		status: false,
-	// 		errors: error.details.map(err => err.message)
-	// 	});
-	// }
-	//
+	const { error } = userSchema.validate(req.body, { abortEarly: false });
+	if (error) {
+		return res.status(400).json({
+			status: false,
+			errors: error.details.map(err => err.message)
+		});
+	}
+
 	const existingUser = await users.findOne({ where: { username: req.body.username } });
 
 
