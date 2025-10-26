3021435e659769f97e0913a8be0f9d769d820dd9
casper <thecaspercraft@gmail.com>
2025-10-26 19:32:24 +0530

    feat: delete user routes

--- backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import userController from '#controllers/userController';
-import { authenticateToken } from '#middlewares/authMiddleware';
-
-const router = express.Router();
-
-router.get('/', authenticateToken, userController.getAllUsers);
-
-export default router
