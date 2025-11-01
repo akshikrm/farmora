e72272464e90d3601521ce2feb2b658bc5c4511f
casper <thecaspercraft@gmail.com>
2025-11-01 21:38:13 +0530

    refactor(subscription): add proper global error handler

--- backend/src/controllers/subscriptionController.js
+++ backend/src/controllers/subscriptionController.js
@@ -1,20 +1,12 @@
-import { PackageNotFoundError } from "#errors/package.errors";
-import { SubsriptionAlreadyActiveError } from "#errors/subscription.errors";
 import subscriptionService from "#services/subscriptionService";
 
 const subscriptionController = {}
 
 subscriptionController.create = async (req, res) => {
-	try {
-		const { package_id } = req.body;
-		const userID = req.user.id;
-		const newSubscription = await subscriptionService.create(userID, package_id);
-		res.success(newSubscription, { message: "Subscription created", statusCode: 201 })
-	} catch (error) {
-		if (error instanceof PackageNotFoundError) { error.statusCode = 404 }
-		if (error instanceof SubsriptionAlreadyActiveError) { error.statusCode = 409 }
-		throw error
-	}
+	const { package_id } = req.body;
+	const userID = req.user.id;
+	const newSubscription = await subscriptionService.create(userID, package_id);
+	res.success(newSubscription, { message: "Subscription created", statusCode: 201 })
 };
 
 subscriptionController.getAll = async (_, res) => {
