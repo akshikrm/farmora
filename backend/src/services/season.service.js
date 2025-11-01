000d0c440e4d33d51663fa6138657b32bd504013
casper <thecaspercraft@gmail.com>
2025-11-01 20:54:42 +0530

    refactor(season): update routing

--- backend/src/services/season.service.js
+++ backend/src/services/season.service.js
@@ -2,11 +2,12 @@ import { SeasonNotFoundError } from "#errors/season.errors";
 import SeasonModel from "#models/season";
 import { Op } from "sequelize";
 
-export const createSeasonService = async (payload) => {
-	return await SeasonModel.create(payload);
+const create = async (payload) => {
+	const newSeason = await SeasonModel.create(payload);
+	return newSeason;
 };
 
-export const getAllSeasonsService = async (payload = {}) => {
+const getAll = async (payload = {}) => {
 	const { page, limit, ...filter } = payload;
 	const offset = (page - 1) * limit;
 
@@ -28,7 +29,7 @@ export const getAllSeasonsService = async (payload = {}) => {
 	};
 };
 
-export const getSeasonByIdService = async (seasonID) => {
+const getByID = async (seasonID) => {
 	const seasonRecord = await SeasonModel.findOne({ where: { id: seasonID } });
 	if (!seasonRecord) {
 		throw new SeasonNotFoundError(seasonID);
@@ -36,13 +37,25 @@ export const getSeasonByIdService = async (seasonID) => {
 	return seasonRecord;
 };
 
-export const updateSeasonService = async (seasonID, payload) => {
-	const seasonRecord = await getSeasonByIdService(seasonID);
+const updateByID = async (seasonID, payload) => {
+	const seasonRecord = await getByID(seasonID);
 	await seasonRecord.update(payload);
 };
 
-export const deleteSeasonService = async (packageID) => {
-	const seasonRecord = await getSeasonByIdService(packageID);
+const deleteByID = async (packageID) => {
+	const seasonRecord = await getByID(packageID);
 	await seasonRecord.destroy();
 };
 
+
+
+const seasonService = {
+	create,
+	getAll,
+	getByID,
+	updateByID,
+	deleteByID,
+}
+
+
+export default seasonService;
