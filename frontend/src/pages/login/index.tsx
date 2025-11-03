bbac2fc521b514816ec0a7fe41fb5abcb13383f5
Casper <thecaspercraft@gmail.com>
2025-11-03 18:47:55 +0530

    feat: add react-hook-form

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -30,26 +30,24 @@ const LoginPage = () => {
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="shadow-md p-8 bg-white rounded min-w-[500px]">
         <h1 className="text-2xl text-center">Login</h1>
-        <div className="flex flex-col gap-4 mt-4">
-          <input
-            className="input"
-            placeholder="username"
-            value={loginData.username}
-            name="username"
-            onChange={onChange}
-          />
-          <input
-            className="input"
-            placeholder="password"
-            value={loginData.password}
-            name="password"
-            type="password"
-            onChange={onChange}
-          />
-          <button className="w-full" onClick={() => onLogin(loginData)}>
-            login
-          </button>
-        </div>
+        <form onSubmit={form.handleSubmit(onLogin)}>
+          <div className="flex flex-col gap-4 mt-4">
+            <input
+              className="input"
+              placeholder="username"
+              {...form.register("username")}
+            />
+            <input
+              className="input"
+              placeholder="password"
+              {...form.register("password")}
+              type="password"
+            />
+            <button className="w-full" type="submit">
+              login
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
