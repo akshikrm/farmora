9d8578a6fbef0948c4be01d302b94fda825e7d65
aventusinformatics <shijin@indconsulting.com>
2025-11-04 10:18:54 +0530

    flutter initial setup

---
+++ app/farmora/lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class ColorUtils{
+  Color primaryColor = Color(0xff453105);
+  Color blackColor = Colors.black;
+  Color whiteColor = Colors.white;
+}
