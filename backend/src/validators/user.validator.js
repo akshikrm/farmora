baf05380d1f30cb357c7f1c041c25325624ff2e6
Casper <thecaspercraft@gmail.com>
2025-11-15 20:40:52 +0530

    fix: username issues

--- backend/src/validators/user.validator.js
+++ backend/src/validators/user.validator.js
@@ -18,10 +18,7 @@ const newStaffMemberSchema = Joi.object({
 
 const updateUserSchema = Joi.object({
   name: Joi.string().min(3).max(100).required(),
-  username: Joi.string().min(3).max(100).required(),
-  password: Joi.string().min(3).max(100).optional(),
   status: Joi.number().integer().required(),
-  package_id: Joi.number().integer().optional(),
 })
 
 const validateNewUser = async (req, res, next) => {
@@ -30,17 +27,6 @@ const validateNewUser = async (req, res, next) => {
     return res.status(400).json({
       status: false,
       errors: error.details.map((err) => err.message),
-    })
-  }
-
-  const existingUser = await users.findOne({
-    where: { username: req.body.username },
-  })
-
-  if (existingUser) {
-    return res.status(400).json({
-      status: false,
-      errors: ['Username already exists. Please choose a different username.'],
     })
   }
 
@@ -56,17 +42,6 @@ export const validateUpdateUser = async (req, res, next) => {
     })
   }
 
-  const existingUser = await users.findOne({
-    where: { username: req.body.username },
-  })
-
-  if (existingUser) {
-    return res.status(400).json({
-      status: false,
-      errors: ['Username already exists. Please choose a different username.'],
-    })
-  }
-
   next()
 }
 
@@ -78,17 +53,6 @@ export const validateNewMember = async (req, res, next) => {
     return res.status(400).json({
       status: false,
       errors: error.details.map((err) => err.message),
-    })
-  }
-
-  const existingUser = await users.findOne({
-    where: { username: req.body.username },
-  })
-
-  if (existingUser) {
-    return res.status(400).json({
-      status: false,
-      errors: ['Username already exists. Please choose a different username.'],
     })
   }
 
