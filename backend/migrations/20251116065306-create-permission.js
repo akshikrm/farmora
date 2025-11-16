63db37dffac3f41e48cf2cec67cfb27e993d4456
Casper <thecaspercraft@gmail.com>
2025-11-16 12:47:56 +0530

    feat: add season permission

---
+++ backend/migrations/20251116065306-create-permission.js
@@ -0,0 +1,32 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('permissions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      key: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('permissions')
+  },
+}
+
---
+++ backend/seeders/20251116065945-permission.js
@@ -0,0 +1,48 @@
+import permissions from '../config/permissions.js'
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('permissions', [
+      {
+        key: permissions.season_read,
+        description: 'Permission to read season data',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        key: permissions.season_write,
+        description: 'Permission to create a new season',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        key: permissions.season_edit,
+        description: 'Permission to edit a new season',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        key: permissions.season_delete,
+        description: 'Permission to delete a season',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete(
+      'permissions',
+      {
+        key: [
+          permissions.season_read,
+          permissions.season_write,
+          permissions.season_edit,
+          permissions.season_delete,
+        ],
+      },
+      {}
+    )
+  },
+}
