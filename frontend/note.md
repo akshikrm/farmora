263fc43e443b1a3c115e290669d7233c9afe2d20
Akshay Krishna <akshikrm@gmail.com>
2026-02-21 11:18:05 +0530

    feat: upgrade frontend to use bun

--- frontend/note.md
+++ frontend/note.md
@@ -1,2 +1,11 @@
 What needs to be done is that while creating a purchase get the base price and
 then add calculate the price based on the base price
+
+- Batch Overview order
+  - Expense
+  - Return
+  - Sale
+  - Performance metrics
+  - Financial Summary
+
+While working on this also check and restructure the frontend code
--- frontend/src/store/authentication/context.ts
+++ frontend/src/store/authentication/context.ts
@@ -1,19 +1,22 @@
-import type { AuthActions, AuthContextData, AuthDispatchContextData } from "@app-types/auth.types";
+import type {
+  AuthActions,
+  AuthContextData,
+  AuthDispatchContextData,
+} from "@app-types/auth.types";
 import { createContext, useContext } from "react";
 
-const noob = (_action: AuthActions) => { }
+const noob = (_action: AuthActions) => {};
 
-export const authDataContext = createContext<AuthContextData>({ 
+export const authDataContext = createContext<AuthContextData>({
   token: null,
   user: null,
 });
 export const authDispatchContext = createContext<AuthDispatchContextData>(noob);
 
 export const useAuth = () => {
-	return useContext(authDataContext);
-}
+  return useContext(authDataContext);
+};
 
 export const useAuthDispatch = () => {
-	return useContext(authDispatchContext);
-}
-
+  return useContext(authDispatchContext);
+};
