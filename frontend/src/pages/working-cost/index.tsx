5531212beb75e7ed6671f8e758fa5a6dfe349df2
Akshay Krishna <akshikrm@gmail.com>
2026-04-09 12:04:09 +0530

    refactor(working-cost): update index.tsx to use hooks pattern

--- frontend/src/pages/working-cost/index.tsx
+++ frontend/src/pages/working-cost/index.tsx
@@ -1,14 +1,23 @@
 import PageTitle from "@components/PageTitle";
 import WorkingCostTable from "./components/table";
 import AddWorkingCost from "./components/add";
+import FilterWorkingCost from "./components/filter";
 import { Button } from "@mui/material";
 import { useState } from "react";
+import useGetWorkingCost from "./hooks/use-get-working-cost";
+import type { WorkingCostFilterRequest } from "./types";
 
 const WorkingCostPage = () => {
   const [isOpen, setOpenAdd] = useState(false);
 
+  const { workingCostList, handleFetchAllWorkingCost } = useGetWorkingCost();
+
   const onOpen = () => setOpenAdd(true);
   const onClose = () => setOpenAdd(false);
+
+  const onFilter = async (inputData: WorkingCostFilterRequest) => {
+    await handleFetchAllWorkingCost(inputData);
+  };
 
   return (
     <>
@@ -18,8 +27,16 @@ const WorkingCostPage = () => {
           Add Working Cost Entry
         </Button>
       </div>
-      <WorkingCostTable />
-      <AddWorkingCost isShow={isOpen} onClose={onClose} />
+      <FilterWorkingCost onFilter={onFilter} />
+      <WorkingCostTable
+        data={workingCostList}
+        onEdit={(id) => console.log("Edit", id)}
+      />
+      <AddWorkingCost
+        isShow={isOpen}
+        onClose={onClose}
+        refetch={handleFetchAllWorkingCost}
+      />
     </>
   );
 };
